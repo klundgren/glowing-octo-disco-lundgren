6a2325189fd280e8e06d9a74f80ffe7c22a14c12
Katie L <klundgren@Katies-MacBook-Pro.local>
2024-06-22 16:06:29 -0400

    sync solution

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,31 @@
 "use strict";
 
-// Print all entries, across all of the sources, in chronological order.
-
 module.exports = (logSources, printer) => {
-  return console.log("Sync sort complete.");
-};
+
+  logSources.filter(logSource => !logSource.drained).forEach(logSource => logSource.pop());
+
+  let logSourcesSortedReverse = logSources.filter(logSource => !logSource.drained).sort((a, b) => b.last.date - a.last.date);
+
+  while (logSourcesSortedReverse.length > 0) {
+
+    const logSourceWithEarliestDate = logSourcesSortedReverse.pop();
+
+    printer.print(logSourceWithEarliestDate.last);
+
+    logSourceWithEarliestDate.pop();
+
+    if (!logSourceWithEarliestDate.drained) {
+      const index = logSourcesSortedReverse.findIndex(logSource => logSource.last.date < logSourceWithEarliestDate.last.date);
+
+      if (index === -1) {
+        logSourcesSortedReverse.push(logSourceWithEarliestDate);
+      } else {
+        logSourcesSortedReverse.splice(index, 0, logSourceWithEarliestDate);
+      }
+    }
+  }
+
+  printer.done();
+
+  return console.log("Sync sort complete.")
+}
